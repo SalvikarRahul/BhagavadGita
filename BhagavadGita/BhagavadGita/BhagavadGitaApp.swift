0dcb5057c1ed074ffacaa9a6f3e3fa5001d37c86
Rahul Salvikar <salvikar.rahul@gmail.com>
2024-03-13 18:29:41 +0530

    Initial commit the code

---
+++ BhagavadGita/BhagavadGita/BhagavadGitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BhagavadGitaApp.swift
+//  BhagavadGita
+//
+//  Created by MYTSP02154 on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BhagavadGitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BhagavadGita/BhagavadGita/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BhagavadGita
+//
+//  Created by MYTSP02154 on 13/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
